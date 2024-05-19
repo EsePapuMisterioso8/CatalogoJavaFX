53d0a288f60db5437722f86d5605d38accedc3ba
pugwhite <elpapafrancisco88@gmail.com>
2024-05-19 13:13:20 -0600

    cambio

--- src/main/java/fes/aragon/inicio/Inicio.java
+++ src/main/java/fes/aragon/inicio/Inicio.java
@@ -25,4 +25,5 @@ public class Inicio extends Application {
     public static void main(String[] args) {
         launch();
     }
+    //wazaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 }
