ea3717ac0206f46a210b5df9067ec8b271d852ed
DeathGYZ <camargojulio127@gmail.com>
2024-05-19 13:29:03 -0600

    Hola Papu

--- src/main/java/fes/aragon/inicio/Inicio.java
+++ src/main/java/fes/aragon/inicio/Inicio.java
@@ -1,5 +1,6 @@
 package fes.aragon.inicio;
 //Ola xd como esta si se ve xd? Wazaaaaaaa
+//Wazaaaaaaaaa
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
