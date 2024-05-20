0210334bde1809405c69c496da2868617beea967
pugwhite <elpapafrancisco88@gmail.com>
2024-05-19 20:19:45 -0600

    cambio circulo

---
+++ src/main/java/fes/aragon/controller/MusicaController.java
@@ -0,0 +1,22 @@
+package fes.aragon.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+
+public class MusicaController {
+
+    @FXML
+    private TableColumn<?, ?> clmAccion;
+
+    @FXML
+    private TableColumn<?, ?> clmCancionNombre;
+
+    @FXML
+    private Pane pnePrincipalmusica;
+
+    @FXML
+    private TableView<?> tblTablamusica;
+
+}
