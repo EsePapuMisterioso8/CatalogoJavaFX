d0c9896adb528ef0be68b028ed990511b21cb631
pugwhite <elpapafrancisco88@gmail.com>
2024-05-19 19:56:12 -0600

    cambio circulo

---
+++ src/main/java/fes/aragon/controller/MenuController.java
@@ -0,0 +1,29 @@
+package fes.aragon.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MenuController {
+
+    @FXML
+    private Button btnabrirjuego;
+
+    @FXML
+    private Button btnmusica;
+
+    @FXML
+    private Pane pneMenu;
+
+    @FXML
+    void accionAbrirjuego(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionPonermusica(ActionEvent event) {
+
+    }
+
+}
