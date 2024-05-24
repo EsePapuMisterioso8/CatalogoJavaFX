9e8dcbea6895c12f18161bca3f438c7bf4a59802
pugwhite <elpapafrancisco88@gmail.com>
2024-05-23 21:47:23 -0600

    cambio tabla

---
+++ src/main/java/fes/aragon/controller/ProyectoController.java
@@ -0,0 +1,100 @@
+package fes.aragon.controller;
+
+import fes.aragon.modelo.ComponentesJuego;
+import fes.aragon.modelo.Persona;
+import fes.aragon.modelo.Personaje;
+import fes.aragon.modelo.SinglentonProyecto;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class ProyectoController {
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private Pane pnePrincipal;
+    private Scene escena;
+    private GraphicsContext graficos;
+    private Personaje personaje;
+    public void setEscena(Scene scene){
+        this.escena =scene;
+    }
+    public void iniciar(){
+        componentesIniciar();
+        cerrarJuego();
+        pintar();
+        eventosTeclado();
+        eventoRaton();
+        ciclo();
+    }
+
+    private void ciclo() {
+        long tiempoInicio = System.nanoTime();
+        AnimationTimer tiempo = new AnimationTimer() {
+            @Override
+            public void handle(long tiempoActual) {
+                double t = (tiempoActual-tiempoInicio)/1000000000.0;
+                calculosLogicos();
+                pintar();
+            }
+
+        };
+        tiempo.start();
+    }
+
+    private void eventoRaton() {
+        escena.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            SinglentonProyecto.getInstance().getFondop().raton(event);
+            }
+        });
+    }
+
+    private void eventosTeclado() {
+        escena.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println("presionado");
+                SinglentonProyecto.getInstance().getPersonaje().teclado(event,true);
+            }
+        });
+        escena.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println("soltado");
+                SinglentonProyecto.getInstance().getPersonaje().teclado(event,false);
+
+            }
+        });
+    }
+
+    private void pintar() {
+        for (ComponentesJuego componentesJuego: SinglentonProyecto.getInstance().getElementos()){
+            componentesJuego.pintar(graficos);
+        }
+
+    }
+
+    private void cerrarJuego() {
+    }
+
+    private void componentesIniciar() {
+        graficos = canvas.getGraphicsContext2D();
+        SinglentonProyecto.getInstance();
+    }
+
+    private void calculosLogicos(){
+       for(ComponentesJuego componentesJuego:SinglentonProyecto.getInstance().getElementos()){
+           componentesJuego.logicaCalculos();
+       }
+    }
+}
